618a90584097788e5801d7c5f469a21164893d84
Warren Yao <warrenyca905@gmail.com>
2019-06-09 00:17:37 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,10 @@
+def ChattyStudent
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+  
+  def raise_hand
+    super
+    i = 0
+    until i == 10
